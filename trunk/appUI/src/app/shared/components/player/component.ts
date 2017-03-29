14b82f8b62034217bdcd765f6446a67aadb445c2
giricgoyal <giricgoyal@gmail.com>
2017-03-30 02:22:34 +0530

    Play Pause

--- trunk/appUI/src/app/shared/components/player/component.ts
+++ trunk/appUI/src/app/shared/components/player/component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'player',
@@ -6,15 +6,33 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
     styleUrls: ['./component.scss']
 })
 
-export class PlayerComponent {
+export class PlayerComponent implements AfterViewInit {
     @Output() closeCallback: EventEmitter<any> = new EventEmitter();
     @Input('inputModel') item: any;
+    @ViewChild('video') videoEl: ElementRef;
 
     constructor() {
 
     }
 
+    ngAfterViewInit() {
+        console.log(this.videoEl);
+    }
+
     close() {
         this.closeCallback.emit(null);
+    }
+
+    playPause(video) {
+        if (this.videoEl.nativeElement.paused) {
+            this.videoEl.nativeElement.play();
+        }
+        else {
+            this.videoEl.nativeElement.pause();
+        }
+    }
+
+    pause() {
+
     }
 }

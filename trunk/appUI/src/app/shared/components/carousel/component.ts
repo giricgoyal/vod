414f1a1d60ea53e9f9f7e18d7e55357145a37205
giricgoyal <giricgoyal@gmail.com>
2017-03-31 00:01:59 +0530

    window resize, keyboard events

--- trunk/appUI/src/app/shared/components/carousel/component.ts
+++ trunk/appUI/src/app/shared/components/carousel/component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, Input, OnChanges, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
     selector: 'carousel',
@@ -16,6 +16,45 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
     private cardCount: number;
     private windowStart: number = 0;
     private showItems: any;
+    private selectedItem: any;
+    private keyboardEv: boolean = false;
+
+    @HostListener('window:keydown', ['$event'])
+    keyboardEvent(event: KeyboardEvent) {
+        if (!this.keyboardEv) {
+            this.selectedItem = {
+                index: this.windowStart,
+                item: this.carouselData[this.windowStart]
+            };
+        }
+        
+        this.keyboardEv = true;
+
+        const leftArrow = 37;
+        const rightArrow = 39;
+        const returnKey = 13;
+
+        if (event.keyCode == leftArrow) {
+            this.selectNextItem(-1);
+        }
+        else if (event.keyCode == rightArrow) {
+            this.selectNextItem(1);
+        }
+        else if(event.keyCode == returnKey) {
+            this.itemClick(this.selectedItem.item);
+        }
+    }
+
+    @HostListener('mousemove', ['$event'])
+    onMousemove(event: MouseEvent) {
+        this.keyboardEv = false;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.ngAfterViewInit();
+        this.initShowItems();
+    }
 
     constructor() {
     }
@@ -23,15 +62,16 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
     ngOnChanges($changesObj) {
         if ($changesObj.carouselData && $changesObj.carouselData.currentValue) {
             this.initShowItems();
+            this.selectedItem = {
+                index: this.windowStart,
+                item: this.carouselData[this.windowStart]
+            };
         }
     }
 
     ngAfterViewInit() {
         this.containerWidth = this.cardContainer.nativeElement.clientWidth;
         this.cardCount = Math.ceil(this.containerWidth / this.cardWidth);
-        if (this.carouselData) {
-            this.initShowItems();
-        }
     }
 
     itemClick(item) {
@@ -42,13 +82,42 @@ export class CarouselComponent implements AfterViewInit, OnChanges {
         this.showItems = [];
         let counter = 0;
         while (counter < this.cardCount) {
-            let index = this.windowStart + counter;
+            let index = this.windowStart + counter >= this.carouselData.length ? (this.windowStart + counter) % this.carouselData.length  : this.windowStart + counter;
             this.showItems.push(this.carouselData[index]);
             counter ++;
         }
     }
 
+    calculateWindowStart(val) {
+        this.windowStart = this.windowStart + val;
+        if (this.windowStart >= this.carouselData.length) {
+            this.windowStart = 0;
+        }
+        else if (this.windowStart < 0) {
+            this.windowStart = this.carouselData.length - 1;
+        }
+    }
+
     scroll(val) { 
+        this.calculateWindowStart(val);
+        this.initShowItems();
+    }
+
+    selectNextItem(val) {
+        var index =  this.selectedItem.index + val > this.carouselData.length - 1 ? 0 : this.selectedItem.index + val;
         
+        if (index < this.windowStart) {
+            this.scroll(val);
+            if (index < 0){ 
+            index = this.windowStart;
+            }
+        }
+        else if (index >= this.windowStart + this.cardCount - 1) {
+            this.scroll(val);
+        }
+        this.selectedItem = {
+            index: index,
+            item: this.carouselData[index]
+        };
     }
 }
